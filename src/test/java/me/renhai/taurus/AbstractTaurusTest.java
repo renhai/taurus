a06630a98a966fa48e94b13652ce0b9fccf7a137
Hai Ren <myrenhai@gmail.com>
2016-11-18 21:14:34 -0800

    remove phantomjs.binary path

--- src/test/java/me/renhai/taurus/AbstractTaurusTest.java
+++ src/test/java/me/renhai/taurus/AbstractTaurusTest.java
@@ -3,9 +3,7 @@ package me.renhai.taurus;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.runner.RunWith;
-import org.openqa.selenium.phantomjs.PhantomJSDriverService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
@@ -21,17 +19,10 @@ public class AbstractTaurusTest {
     @Autowired
     protected MockMvc mockMvc;
     
-	@Value("${phantomjs.binary.path}")
-	private String phantompath;
-	
-	private static boolean initialized;
     
     @Before
     public void before() {
-    	if (!initialized) {
-    		System.setProperty(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY, phantompath);
-    		initialized = true;
-    	}
+    	
     }
     
     @After
