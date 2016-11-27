f7b9beaa2a80ef60cfa9b82e487eb0d5cfc559ec
Hai Ren <myrenhai@gmail.com>
2016-11-27 12:14:15 -0800

    remove start and stop script

--- start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd /root/workspace/taurus/
-git pull origin master
-mvn clean package -DskipTests
-nohup java -Xms712m -Xmx712m -Dspring.profiles.active=prod -Dphantomjs.binary.path=/usr/local/phantomjs-2.1.1-linux-x86_64/bin/phantomjs -jar target/taurus-0.0.1.jar >./log/nohup.log 2>&1 &
--- stop.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' 'http://localhost:8080/shutdown'
